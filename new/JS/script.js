b7a8e146ec4daa1a91f7c6782aaf48ba4a430099
Jellewijma <jelle.wijma@gmail.com>
2021-04-01 13:20:08 +0200

    card scroll DONE

--- new/JS/script.js
+++ new/JS/script.js
@@ -10,13 +10,21 @@ $(document).ready(function () {
 
   window.onscroll = function() {scrollFunction()};
 
+  console.log(window.innerHeight)
+  window.scrollTo(0,0);
   function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("pusher-top").style.fontSize = "30px";
-      document.getElementById("pusher-bottom").style.fontSize = "30px";
-    } else {
-      document.getElementById("pusher-top").style.fontSize = "30px";
-      document.getElementById("pusher-bottom").style.fontSize = "30px";
-    }
+    // get the current scroll position
+      var scrollPosition = document.documentElement.scrollTop
+    // calc the height that needs to be added
+      var addheight = (scrollPosition / window.innerHeight) * 2 * 100
+
+      if (addheight < 100) {
+        var plus = 50 + addheight / 2
+        var minus = 50 - addheight / 2
+        // add height to the top div
+        document.getElementById('pusher-top').style.height = `calc(${plus}vh - 120px)`;
+        // remove height to the lower div
+        document.getElementById('pusher-bottom').style.height = `calc(${minus}vh - 120px)`;
+      }
   }
 });
