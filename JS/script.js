f13fbd38b68b7451380d8f6b6e2512cab215674d
Jellewijma <jelle.wijma@gmail.com>
2021-05-25 12:44:29 +0200

    UPDATE 2

--- JS/script.js
+++ JS/script.js
@@ -28,4 +28,31 @@ $(document).ready(function () {
       }
   }
 
+  document.getElementById("back").addEventListener("click", function close() {
+    console.log("test");
+    document.getElementById('background-new-level').style.height = `0px`;
+    document.getElementById('background-new-level').style.width = `0px`;
+    document.body.style.overflow = "auto";
+  });
+
+
 });
+
+function page() {
+  var ca2 = top.innerHeight * top.innerHeight;
+  var ab2 = top.innerWidth * top.innerWidth;
+
+  var bc2 = ca2 + ab2;
+
+  var diagle = Math.sqrt(bc2);
+
+  console.log(diagle);
+
+  document.getElementById('background-new-level').style.height = `100vh`;
+  document.getElementById('background-new-level').style.width = `100vw`;
+
+  // document.getElementById('background-new-level').style.height = `${diagle}px`;
+  // document.getElementById('background-new-level').style.width = `${diagle}px`;
+  // document.getElementById('background-new-level').style.borderRadius = "0%";
+  document.body.style.overflow = "hidden";
+}
